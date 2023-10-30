fe7c43abb02be6f9d8c36737590b3b9e25ca29bb
12z3 <77529497+12z3@users.noreply.github.com>
2023-10-30 17:18:09 +0200

    Add files via upload

---
+++ Random.js
@@ -0,0 +1,11 @@
+
+for (let j = 0; j <= 5; j++) {
+    let a = [];
+    for (let i = 1; i <= 6; i++) {
+        let el = (Math.floor(Math.random() * 50) + 1);
+        //console.log(el);
+        a.push(el);
+    }
+    console.log(`${a} + \n`);
+}
+
