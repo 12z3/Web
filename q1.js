683081d26612e573192a79932db1c77cdbbb12bf
12z3 <77529497+12z3@users.noreply.github.com>
2023-11-02 08:13:26 +0200

    Add files via upload

---
+++ q1.js
@@ -0,0 +1,29 @@
+let a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+//[[1,2,3], [4,5,6], [7,8,9]] === 1,2,3,4,5,6,7,8,9 ;)))))
+let b = [];
+let el = 9;
+let size = a.length;
+
+
+for (let i = 0; i < size; i++) {
+    let idx = 0;
+    if (a[i].includes(9)) {
+        idx = a.indexOf(9);
+        if (i !== idx) {
+            console.log("x");
+        } else console.log(`${a[idx]}`)
+    } else {
+        a[i] = ["x", "x", "x"];
+        console.log(`${a[i]}`);
+    }
+}
+
+for(let i = 0; i < a[0].length){
+    for(let j = 0; j < a[0].length){
+
+        
+
+    }
+}
+
+
